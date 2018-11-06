e1b1d828d45f133af66a6c435f2c868a91c44f54
Nick Kasten <kastentx@gmail.com>
2018-11-06 14:52:05 -0600

    generalized utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,65 +5,15 @@ const KUBE_MODEL_PORT = process.env.REACT_APP_KUBE_MODEL_PORT || ''
 const LOCAL_MODEL_PORT = process.env.REACT_APP_LOCAL_MODEL_PORT || 5000
 const DEPLOY_TYPE = process.env.REACT_APP_DEPLOY_TYPE || ''
 
-export const OBJ_LIST = ['background', 'airplane', 'bicycle', 'bird', 'boat', 
-'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'dining table', 
-'dog', 'horse', 'motorbike', 'person', 'potted plant', 'sheep', 
-'sofa', 'train', 'tv']
+export const isNonEmpty = obj => obj && Object.keys(obj).length !== 0
 
-let objMap = {} 
-OBJ_LIST.forEach((x,i)=> objMap[x]=i)
-export const OBJ_MAP = objMap
+export const isEmpty = obj => !isNonEmpty(obj)
 
-export const COLOR_MAP = {
-  green: [0, 128, 0],
-  red: [255, 0, 0],
-  blue: [0, 0, 255],
-  purple: [160, 32, 240],
-  pink: [255, 185, 80],
-  teal: [0, 128, 128],
-  yellow: [255, 255, 0],
-  gray: [192, 192, 192]
-}
+export const cleanMAXResponse = response => response.data.prediction
 
-export const COLOR_LIST = Object.values(COLOR_MAP)
-
-export const getColor = pixel => COLOR_LIST[pixel - 1]
-
-export const isNonEmpty = obj => {
-  return obj && Object.keys(obj).length !== 0
-}
-
-export const isEmpty = obj => {
-  return !isNonEmpty(obj)
-}
-
-export const cleanDocs = docs => {
-  return docs.rows.map(
-    doc=> {
-      const segList = Object.keys(doc.doc._attachments)
-      let segObject = {}
-      for (let seg of segList) {
-        segObject[seg] = { 
-          name: seg,
-          hasData: doc.doc._attachments[seg] && true
-        }
-      }
-      return {
-        id: doc.doc._id,
-        rev: doc.value.rev,
-        width: doc.doc.width,
-        height: doc.doc.height,
-        segments: segObject
-      }
-    }
-  )
-}
-
-export const OGpredict = img => {
+export const modelCheck = () => {
   let modelPort
   let modelIP
-  let bodyFormData = new FormData()
-  bodyFormData.set('text', img)
   if (DEPLOY_TYPE === 'KUBE') {
     modelPort = KUBE_MODEL_PORT
     modelIP = KUBE_MODEL_IP
@@ -72,10 +22,9 @@ export const OGpredict = img => {
     modelIP = 'localhost'
   }
   return axios({
-    method: 'post',
-    url: `http://${modelIP}:${modelPort}/model/predict`,
-    data: bodyFormData,
-    config: { headers: { 'Content-Type' : 'multipart/form-data', 'accept' : 'application/json' }, timeout: 8000 }
+    method: 'get',
+    url: `http://${modelIP}:${modelPort}/model/metadata`,
+    config: { headers: { 'Content-Type' : 'application/json', 'accept' : 'application/json' }, timeout: 8000 }
   })
 }
 
@@ -98,13 +47,11 @@ export const predict = text => {
   })
 }
 
-export const cleanMAXResponse = response => {
-  return response.data.prediction
-}
-
-export const modelCheck = () => {
+export const imgPredict = img => {
   let modelPort
   let modelIP
+  let bodyFormData = new FormData()
+  bodyFormData.set('text', img)
   if (DEPLOY_TYPE === 'KUBE') {
     modelPort = KUBE_MODEL_PORT
     modelIP = KUBE_MODEL_IP
@@ -113,8 +60,9 @@ export const modelCheck = () => {
     modelIP = 'localhost'
   }
   return axios({
-    method: 'get',
-    url: `http://${modelIP}:${modelPort}/model/metadata`,
-    config: { headers: { 'Content-Type' : 'application/json', 'accept' : 'application/json' }, timeout: 8000 }
+    method: 'post',
+    url: `http://${modelIP}:${modelPort}/model/predict`,
+    data: bodyFormData,
+    config: { headers: { 'Content-Type' : 'multipart/form-data', 'accept' : 'application/json' }, timeout: 8000 }
   })
 }
