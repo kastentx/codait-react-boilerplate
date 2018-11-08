e3c399bd156e190664a632d5a53180c89c6dd77c
Nick Kasten <kastentx@gmail.com>
2018-11-08 11:23:44 -0600

    utilities refactored

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,25 +6,24 @@ export const isEmpty = obj => !isNonEmpty(obj)
 
 export const cleanMAXResponse = response => response.data.prediction
 
-export const modelCheck = (modelPort=5000, modelIP='localhost') => {
-  return axios({
+export const modelCheck = (modelPort=5000, modelIP='localhost') => (
+  axios({
     method: 'get',
     url: `http://${modelIP}:${modelPort}/model/metadata`,
     config: { headers: { 'Content-Type' : 'application/json', 'accept' : 'application/json' }, timeout: 8000 }
   })
-}
+)
 
-export const predict = (textInput, modelPort=5000, modelIP='localhost') => {
-  return axios({
+export const predict = (text, modelPort=5000, modelIP='localhost') => (
+  axios({
     method: 'post',
     url: `http://${modelIP}:${modelPort}/model/predict`,
-    data: textInput,
+    data: { text },
     config: { headers: { 'Content-Type' : 'application/json', 'accept' : 'application/json' }, timeout: 8000 }
   })
-}
+)
 
 export const imgPredict = (img, modelPort=5000, modelIP='localhost') => {
-
   let bodyFormData = new FormData()
   bodyFormData.set('image', img)
   bodyFormData.set('type', img.content_type)
