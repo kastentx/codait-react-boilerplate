c07a5436e5ccca8e74bfd7df53383090b8ea6341
Nick Kasten <kastentx@gmail.com>
2018-11-08 11:24:37 -0600

    refactored textInput

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,4 +1,4 @@
-import { Row, Col, Form, FormGroup, Input, Label, Button, FormText, Container } from 'reactstrap'
+import { Row, Col, Form, FormGroup, Input, Label, Button } from 'reactstrap'
 import React from 'react'
 
 const TextInput = props => (
@@ -29,19 +29,6 @@ const TextInput = props => (
         </Form>
       </Col>
 
-      <Col>
-        <Form>
-          <FormGroup>
-            <Label for="outputArea">Model Output</Label>
-            <Input 
-              type="textarea" 
-              name="text" 
-              id="outputArea" 
-              value={ props.MAXOutput ? JSON.stringify(props.MAXOutput) : '' }
-              readOnly />
-          </FormGroup>
-        </Form>
-      </Col>
     </Row>  
   </div>
 )
