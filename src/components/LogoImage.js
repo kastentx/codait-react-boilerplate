9f742b9a710cc116158931a3c77bf605dbcbaead
Nick Kasten <kastentx@gmail.com>
2018-11-08 11:24:46 -0600

    refactored logoImage

--- src/components/LogoImage.js
+++ src/components/LogoImage.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { Row } from 'reactstrap'
 
 const LogoImage = props => (
-  <Row className="topRow">
-      <img src={ props.image } className="App-logo" alt="logo" />
-  </Row>
+  <img src={ props.image } className="App-logo" alt="logo" />
 )
 
 export default LogoImage
