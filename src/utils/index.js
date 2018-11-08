14a75648badd5572d5d8582edcd6ee08d6ede0c7
Nick Kasten <kastentx@gmail.com>
2018-11-08 10:41:24 -0600

    refactored utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,26 +1,12 @@
 import axios from 'axios'
 
-const KUBE_MODEL_IP = process.env.REACT_APP_KUBE_IP || ''
-const KUBE_MODEL_PORT = process.env.REACT_APP_KUBE_MODEL_PORT || ''
-const LOCAL_MODEL_PORT = process.env.REACT_APP_LOCAL_MODEL_PORT || 5000
-const DEPLOY_TYPE = process.env.REACT_APP_DEPLOY_TYPE || ''
-
 export const isNonEmpty = obj => obj && Object.keys(obj).length !== 0
 
 export const isEmpty = obj => !isNonEmpty(obj)
 
 export const cleanMAXResponse = response => response.data.prediction
 
-export const modelCheck = () => {
-  let modelPort
-  let modelIP
-  if (DEPLOY_TYPE === 'KUBE') {
-    modelPort = KUBE_MODEL_PORT
-    modelIP = KUBE_MODEL_IP
-  } else {
-    modelPort = LOCAL_MODEL_PORT
-    modelIP = 'localhost'
-  }
+export const modelCheck = (modelPort=5000, modelIP='localhost') => {
   return axios({
     method: 'get',
     url: `http://${modelIP}:${modelPort}/model/metadata`,
@@ -28,37 +14,21 @@ export const modelCheck = () => {
   })
 }
 
-export const predict = text => {
-  let modelPort
-  let modelIP
-  let formData = { text }
-  if (DEPLOY_TYPE === 'KUBE') {
-    modelPort = KUBE_MODEL_PORT
-    modelIP = KUBE_MODEL_IP
-  } else {
-    modelPort = LOCAL_MODEL_PORT
-    modelIP = 'localhost'
-  }
+export const predict = (textInput, modelPort=5000, modelIP='localhost') => {
   return axios({
     method: 'post',
     url: `http://${modelIP}:${modelPort}/model/predict`,
-    data: formData,
+    data: textInput,
     config: { headers: { 'Content-Type' : 'application/json', 'accept' : 'application/json' }, timeout: 8000 }
   })
 }
 
-export const imgPredict = img => {
-  let modelPort
-  let modelIP
+export const imgPredict = (img, modelPort=5000, modelIP='localhost') => {
+
   let bodyFormData = new FormData()
-  bodyFormData.set('text', img)
-  if (DEPLOY_TYPE === 'KUBE') {
-    modelPort = KUBE_MODEL_PORT
-    modelIP = KUBE_MODEL_IP
-  } else {
-    modelPort = LOCAL_MODEL_PORT
-    modelIP = 'localhost'
-  }
+  bodyFormData.set('image', img)
+  bodyFormData.set('type', img.content_type)
+
   return axios({
     method: 'post',
     url: `http://${modelIP}:${modelPort}/model/predict`,
