27b41ba368422e7f99337c2c0ec120f868f0af98
Nick Kasten <kastentx@gmail.com>
2018-11-08 10:41:17 -0600

    added disconnected Model msg

--- src/components/ModelData.js
+++ src/components/ModelData.js
@@ -3,16 +3,30 @@ import { Row, FormText } from 'reactstrap'
 
 
 const ModelData = props => (
-  <div>
-    <Row className="midRow">
-      { props.modelType ? props.modelType.name : null }
-    </Row>
-    <Row>
-      <FormText>
-        { props.modelType ? props.modelType.description : null }
-      </FormText>
-    </Row>  
-  </div>
+  props.modelType ? 
+    <div>
+      <Row className="midRow">
+        { props.modelType.name }
+      </Row>
+      <Row>
+        <FormText>
+          { props.modelType.description }
+        </FormText>
+      </Row>  
+    </div>
+  :
+    <div>
+      <Row>
+        {`No MAX Model found running at Port ${5000444}`}
+      </Row>
+      <Row>
+        <FormText>
+          { `If the model is running on another port, edit the value in ` }
+          <code>App.js</code>
+          { ` and reload.` }
+        </FormText>
+      </Row>
+    </div>  
 )
 
 export default ModelData
